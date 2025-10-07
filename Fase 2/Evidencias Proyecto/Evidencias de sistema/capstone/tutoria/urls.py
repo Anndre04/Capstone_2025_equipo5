bc0ad8d9a32f856acd55d4847a5d3f91913b11a1
ignaciobellov-bit <ignaciobellov@gmail.com>
2025-10-07 00:16:52 -0300

    ajustes base y buscador

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/capstone/tutoria/urls.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/capstone/tutoria/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('solicitudesprof/<int:user_id>', views.solicitudesprof, name='solicitudesprof'),    
     path('solicitud/<int:solicitud_id>/aceptar/', views.aceptar_solicitud, name='aceptar_solicitud'),
     path('solicitud/<int:solicitud_id>/rechazar/', views.rechazar_solicitud, name='rechazar_solicitud'),
+    path('gestortutorias', views.gestortutorias, name='gestortutorias'),
+    
+    
+    
 
 ]
