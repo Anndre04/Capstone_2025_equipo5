828e6c32066ddb9b6e4d48bb4e0e062d3bfa6706
ignaciobellov-bit <ignaciobellov@gmail.com>
2025-10-07 22:20:07 -0300

    correcion registro y perfil

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/capstone/tutoria/urls.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/capstone/tutoria/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('solicitud/<int:solicitud_id>/aceptar/', views.aceptar_solicitud, name='aceptar_solicitud'),
     path('solicitud/<int:solicitud_id>/rechazar/', views.rechazar_solicitud, name='rechazar_solicitud'),
     path('gestortutorias', views.gestortutorias, name='gestortutorias'),
-    
+    path('perfil', views.perfil, name='perfil'),
+    path('perfilusuario', views.perfilusuario, name='perfilusuario'),
     
     
 
